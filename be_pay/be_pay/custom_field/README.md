3f914d8a33e3803c3df94bb274b5d6124be167c4
Ointech <ointech.rdcongo@gmail.com>
2026-05-16 10:58:47 +0100

    New Custom

---
+++ be_pay/be_pay/custom_field/README.md
@@ -0,0 +1,6 @@
+# Custom Fields
+
+Les custom fields du module Be Pay sont désormais gérés via les fixtures :
+`apps/be_pay/be_pay/fixtures/custom_field.json`
+
+Utilisez `bench export-fixtures` pour les synchroniser.
--- be_pay/be_pay/doctype/pay_employment_type_list/test_pay_employment_type_list.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, ebamadernis@gmail.com and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestPayEmploymentTypeList(FrappeTestCase):
-    pass
